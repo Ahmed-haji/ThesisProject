5fbb6e34465e371ffc7c93f5bdeff2d6512b64f4
Ahmed-haji <128056511+Ahmed-haji@users.noreply.github.com>
2024-04-28 13:40:02 +0100

    core funcs

--- functions.py
+++ functions.py
@@ -4,16 +4,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import norm
 
-def newton_step(f,x0):
-    def df(x):
-        dx = 1e-8
-        return (f(x + dx) - f(x))/dx
-    return x0 - f(x0)/df(x0)
-def newton (f,x0, tol=1e-6):
-    x = x0
-    while abs(f(x)) > tol:
-        x = newton_step(f,x)
-    return x
+def vega(S, K, t, r, sigma):
+    d1 = (np.log(S / K) + (r + 0.5 * sigma ** 2) * t) / (sigma * np.sqrt(t))
+    vega = S * norm.pdf(d1) * np.sqrt(t)
+    return vega
+
+def inflection_point(S, K, t, r):
+    m = S/(K*np.exp(-r*t))
+    return np.sqrt(2*np.abs(np.log(m)) / t)
+def implied_volatility(C,S,K, t, r, price):
+    MAX_ITERATIONS = 100
+    PRECISION = 10e-7
+
+    x0 = inflection_point(S, K, t, r)
+    for i in range(0, MAX_ITERATIONS):
+        price_calculated = black_scholes_call(S, K, t, r, x0)
+        vega_calculated = vega(S, K, t, r, x0)
+
+        price_difference = price - price_calculated
+
+        if abs(price_difference) < PRECISION:
+            return
+
+        x0 = x0 + price_difference / vega_calculated
+    return x0
+
+
 
 def black_scholes_call(s, k, t, r, sigma):
     """
@@ -36,30 +52,7 @@ def black_scholes_call(s, k, t, r, sigma):
     d2 = d1 - sigma * np.sqrt(t)
 
     call_price = s * norm.cdf(d1) - k * np.exp(-r * t) * norm.cdf(d2)
-    print("car")
     return call_price
 
-def black_scholes_put(s, k, t, r, sigma):
-    """
-    Calculate the price of a European call option using the Black-Scholes model.
 
-    Parameters:
-    s (float): Current price of the underlying asset (stock price).
-    k (float): Strike price of the option (price at which the option can be exercised).
-    tau (float): Time to maturity of the option, days.
-    t (float): Current time in the context of the option's life (usually 0 if at valuation).
-    r (float): Risk-free interest rate
-    sigma (float): Volatility of the underlying asset, expressed as a decimal.
-
-    Returns:
-    float: The price of the call/put
-     option.
-    """
-
-    d1 = (np.log(s / k) + (r + 0.5 * sigma ** 2) * t) / (sigma * np.sqrt(t))
-    d2 = d1 - sigma * np.sqrt(t)
-
-    call_price = s * norm.cdf(d1) - k * np.exp(-r * t) * norm.cdf(d2)
-    print("car")
-    return call_price
 
