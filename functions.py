837d37198bbff63d265037c9780f075eaba9f8b7
Ahmed-haji <128056511+Ahmed-haji@users.noreply.github.com>
2024-04-28 13:06:42 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -39,4 +39,27 @@ def black_scholes_call(s, k, t, r, sigma):
     print("car")
     return call_price
 
+def black_scholes_put(s, k, t, r, sigma):
+    """
+    Calculate the price of a European call option using the Black-Scholes model.
+
+    Parameters:
+    s (float): Current price of the underlying asset (stock price).
+    k (float): Strike price of the option (price at which the option can be exercised).
+    tau (float): Time to maturity of the option, days.
+    t (float): Current time in the context of the option's life (usually 0 if at valuation).
+    r (float): Risk-free interest rate
+    sigma (float): Volatility of the underlying asset, expressed as a decimal.
+
+    Returns:
+    float: The price of the call/put
+     option.
+    """
+
+    d1 = (np.log(s / k) + (r + 0.5 * sigma ** 2) * t) / (sigma * np.sqrt(t))
+    d2 = d1 - sigma * np.sqrt(t)
+
+    call_price = s * norm.cdf(d1) - k * np.exp(-r * t) * norm.cdf(d2)
+    print("car")
+    return call_price
 
