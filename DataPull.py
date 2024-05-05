f67a8b727f249f7f324774bc25fbb72b1a4626c0
Ahmed-haji <128056511+Ahmed-haji@users.noreply.github.com>
2024-05-05 20:28:24 +0100

    methods

--- DataPull.py
+++ DataPull.py
@@ -8,6 +8,10 @@ def pull_example_data():
     print(df)
     return df
 
+def pull_google_2018():
+    df = pd.read_json(r'C:\Thesis\Datasets\option_data-main\google_2018.json.bz2', compression='bz2', orient='index')
+    return df
+
 df = pd.read_json(r'C:\Thesis\Datasets\option_data-main\alphabet_2018.json.bz2', compression='bz2', orient='index')
 print(df)
 
---
+++ selection.py
@@ -0,0 +1,21 @@
+## selection.py
+## this script will select the appropriate options using a method derived from VIX near and next term selection methods
+import numpy as np
+import pandas as pd
+import DataPull
+import functions
+
+
+def filter_options(df, time_to_maturity=(24, 40)):
+    df = df[(df['time_to_maturity'] >= 20) & (
+                df['time_to_maturity'] <= 40)]  # filters options with time to maturity between 20 and 40 days
+
+    atm_prices = df.groupby('date')[
+        'forward_price'].median().reset_index()  # calculates the median forward price for each date .reset_index() is used to convert series to a dataframe
+    atm_prices = atm_prices.rename(columns={'forward_price': 'atm_price'})
+
+    df = pd.merge(df, atm_prices, on='date', how='left')  # places atm prices on main dataframe
+
+    df['moneyness'] = (df['strike_price'] / df['atm_price'])  # calculates the  moneyness
+    df = df[(df['moneyness'] <= 1.1])&(df['moneyness'] >= 0.9)]  # filters options with moneyness between 0.9 and 1.1
+    return  df
