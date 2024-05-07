03c2e89c56e8412053b48c4945514d345bc61ae1
Ahmed-haji <128056511+Ahmed-haji@users.noreply.github.com>
2024-05-07 10:46:05 +0100

    1321

--- DataPull.py
+++ DataPull.py
@@ -9,9 +9,7 @@ def pull_example_data():
     return df
 
 def pull_google_2018():
-    df = pd.read_json(r'C:\Thesis\Datasets\option_data-main\google_2018.json.bz2', compression='bz2', orient='index')
+    df = pd.read_json(r'C:\Thesis\Datasets\option_data-main\alphabet_2018.json.bz2', compression='bz2', orient='index')
     return df
 
-df = pd.read_json(r'C:\Thesis\Datasets\option_data-main\alphabet_2018.json.bz2', compression='bz2', orient='index')
-print(df)
 
--- functions.py
+++ functions.py
@@ -3,27 +3,42 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import norm
 
+
 def vega(S, K, t, r, sigma):
     d1 = (np.log(S / K) + (r + 0.5 * sigma ** 2) * t) / (sigma * np.sqrt(t))
     vega = S * norm.pdf(d1) * np.sqrt(t)
     return vega
 
-def inflection_point(S, K, t, r):
-    m = S/(K*np.exp(-r*t))
-    return np.sqrt(2*np.abs(np.log(m)) / t)
 
-def implied_volatility(C:int, S, K, t, r, tol = 1e-5, max_iter =100) -> float:
-    x0 = inflection_point(S, K, t, r)
-    p = black_scholes_call(S, K, t, r, x0)
-    v = vega(S, K, t, r, x0)
-    while (abs(p - C)/v) > tol and max_iter > 0:
+def inflection_point(S, K, t, r):
+    m = S / (K * np.exp(-r * t))
+    return np.sqrt(2 * np.abs(np.log(m)) / t)
+
+
+def implied_volatility(C, s, K, t, r, tol=1e-5, max_iter=100) -> float:
+    """
+    Calculate the implied volatility of a European call option using the Black-Scholes model.
+    Args:
+        C:  The market price of the option
+        s:
+        K:
+        t:
+        r:
+        tol:
+        max_iter:
+
+    Returns:
+
+    """
+    x0 = inflection_point(s, K, t, r)
+    p = black_scholes_call(s, K, t, r, x0)
+    v = vega(s, K, t, r, x0)
+    while (abs(p - C) / v) > tol and max_iter > 0:
         x0 = x0 - (p - C) / v
-        p = black_scholes_call(S, K, t, r, x0) # theoretical price of the option
-        v = vega(S, K, t, r, x0)
+        p = black_scholes_call(s, K, t, r, x0)  # theoretical price of the option
+        v = vega(s, K, t, r, x0)
         max_iter -= 1
     return x0
-
-
 
 
 def black_scholes_call(s, k, t, r, sigma):
@@ -48,6 +63,3 @@ def black_scholes_call(s, k, t, r, sigma):
 
     call_price = s * norm.cdf(d1) - k * np.exp(-r * t) * norm.cdf(d2)
     return call_price
-
-
-
