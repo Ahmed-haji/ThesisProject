f3ca8f8cc73d72dd6b26c489f9a873f8b8770651
Ahmed-haji <128056511+Ahmed-haji@users.noreply.github.com>
2024-07-06 14:12:27 +0100

    model evals

--- DataProcess.py
+++ DataProcess.py
@@ -65,8 +65,5 @@ def preprocess_data(data, history_name, seq_length=60, splits=5, window=21):
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
 
-    # reshape data for xgboost
-    # X_train = X_train.reshape(X_train.shape[0], X_train.shape[1] * X_train.shape[2])
-    # X_test = X_test.reshape(X_test.shape[0], X_test.shape[1] * X_test.shape[2])
 
     return X_train, X_test, y_train, y_test
